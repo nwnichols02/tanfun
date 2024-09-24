3ff2d462630727285784f09f6daef6efabdcc0fc
Nate Nichols <nate.nichols@utili-source.com>
2024-09-23 21:35:45 -0600

    initial commit

---
+++ app/client.tsx
@@ -0,0 +1,9 @@
+// app/client.tsx
+import React from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { StartClient } from '@tanstack/start'
+import { createRouter } from './router'
+
+const router = createRouter()
+
+hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
